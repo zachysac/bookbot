65677d80def8dffe082c75dafdd002d019ca0b17
zachysac <zachsmithley@icloud.com>
2025-01-18 14:34:40 -0700

    added flavor text to sentence syntax to word_count and began to build letter count

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-def main():
+def word_count():
     with open("books/frankenstein.txt") as f:
         count=0
         file_contents = f.read()
         words = file_contents.split( )
         for word in words:
             count += 1
-        print(count)
-main()
+        print(f"There are {count} words in the text.")
+word_count()
+
+##this is not working
+##looking for solution to increase count of letter in the dictionary
+def letter_count():
+     with open("books/frankenstein.txt") as f:
+        count=0
+        file_contents = f.read()
+        lowered_string = file_contents.lower()
+        for letters in lowered_string:
+            letters = {
+                "a": +=1,
+             "b": +=1,
+             "c": +=1
+         }
+        print(letters)
+letter_count()

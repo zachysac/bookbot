ecfa6b4c05c12c4a9ef63f8b3811086b960b0789
zachysac <zachsmithley@icloud.com>
2025-01-18 14:04:14 -0700

    Built word counter in main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def main():
     with open("books/frankenstein.txt") as f:
+        count=0
         file_contents = f.read()
-        print(file_contents)
+        words = file_contents.split( )
+        for word in words:
+            count += 1
+        print(count)
 main()
